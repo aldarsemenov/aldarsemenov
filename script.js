ad39d04d1b70a9bb0c4965f3e011f421e61bf7f1
Aldar <aldarsemenov@mail.ru>
2023-02-12 22:23:46 +0800

    fix

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let pasta = {
-    ing1: makaron,
-    ing2: cheese,
-    ing3: milk,
-    ing4:chicken,
+    ing1: 'makaron',
+    ing2: 'cheese',
+    ing3: 'milk',
+    ing4:'chicken',
     costPrice:100 
 }
 let zharexa = {   
      //жареха
-     ing1: potato,
-     ing2: onion,
-     ing3: pork,
+     ing1: 'potato',
+     ing2: 'onion',
+     ing3: 'pork',
     costPrice:90
 }
 let  booza ={
-    ing1: farsh,
-    int2: testo,
+    ing1: 'farsh',
+    int2: 'testo',
 
     price: 55
 }
 let buchler  = {  //бухлер
-    ing1: beef,
-    ing2: potato,
+    ing1: 'beef',
+    ing2: 'potato',
     price: 80
 }
 
