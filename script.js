cf2e8a7bcb70315089b221ddcb18ebc3579765b0
Aldar <aldarsemenov@mail.ru>
2023-02-12 23:28:04 +0800

    fix

--- script.js
+++ script.js
@@ -3,28 +3,50 @@ let pasta = {
     ing2: 'cheese',
     ing3: 'milk',
     ing4:'chicken',
-    costPrice:100 
+    costPrice:70, //себестоимость
+    cost: 180      //цена
 }
 let zharexa = {   
      //жареха
      ing1: 'potato',
      ing2: 'onion',
      ing3: 'pork',
-    costPrice:90
+    costPrice:90,
+    cost: 190
 }
 let  booza ={
     ing1: 'farsh',
     int2: 'testo',
-
-    price: 55
+    costPrice: 25,
+    cost: 55
 }
 let buchler  = {  //бухлер
     ing1: 'beef',
     ing2: 'potato',
-    price: 80
+    costPrice: 100,
+    cost: 230
 }
 
-let menu = [pasta,zharexa,booza,buchler];
+let menu = [pasta,zharexa,booza,buchler]; // создание массива
 
-alert(JSON.stringify(menu[0]));
+
+menu.shift(); // удаление первого элемента и сдвиг назад
+
+delete pasta.ing2; // удаление ингридиента из обьекта паста
+menu.unshift(pasta); //добавление обьекта паста в массив 
+
+
+// доход с продажи одной порции
+let dohod =  `Выручка за продажу одной порции блюда   
+Паста = ${pasta.cost-pasta.costPrice}
+Жареха = ${zharexa.cost-zharexa.costPrice}
+Бууза = ${booza.cost-booza.costPrice}
+Бухлеор = ${buchler.cost-buchler.costPrice}`;
+console.log(dohod);
+
+
+//удаление данных с массива
+menu.shift(pasta,booza,buchler);
+
+alert(dohod);
 
