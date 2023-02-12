d50e25494e0262b6b009333403a7540d164d5859
Aldar <aldarsemenov@mail.ru>
2023-02-12 22:20:28 +0800

    hw7

---
+++ script.js
@@ -0,0 +1,30 @@
+let pasta = {
+    ing1: makaron,
+    ing2: cheese,
+    ing3: milk,
+    ing4:chicken,
+    costPrice:100 
+}
+let zharexa = {   
+     //жареха
+     ing1: potato,
+     ing2: onion,
+     ing3: pork,
+    costPrice:90
+}
+let  booza ={
+    ing1: farsh,
+    int2: testo,
+
+    price: 55
+}
+let buchler  = {  //бухлер
+    ing1: beef,
+    ing2: potato,
+    price: 80
+}
+
+let menu = [pasta,zharexa,booza,buchler];
+
+alert(menu);
+
