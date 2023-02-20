495dfc2c546eb2bbd66619c7343066b6060c618f
Aldar <aldarsemenov@mail.ru>
2023-02-20 23:47:28 +0800

    add

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let age = prompt('What is your age?');
-if (age >=21) {
-    alert('You can drink alcohol');
+let schetchik;
+let name = prompt('Как вас зовут?');
+alert(name +'Добро пожаловать на мой квиз');
+
+let question = prompt ('Первый вопрос. Назовите столицу Италии')
+
+if (question ='Рим') {
+    alert('Верный ответ');
+    schetchik= schetchik+1;
 }
 else {
-    alert('ne mojew pit')
+    alert('Ошибочка')
 }
+let question2 = prompt ('Назовите столицу Японии')
+if (question2 ='Япония') {
+    alert('Верный ответ');
+    schetchik= schetchik+1;
+}
+else {
+    alert('Ошибочка')
+}
+
+
+alert('Количество правильных ответов'+schetchik);
