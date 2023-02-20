18cf71ef7acdbdac537b26bdfe2ff9ad267f5d1f
Aldar <aldarsemenov@mail.ru>
2023-02-20 23:53:38 +0800

    ашч

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let schetchik;
+let schetchik = 0;
 let name = prompt('Как вас зовут?');
-alert(name +'Добро пожаловать на мой квиз');
+alert(name +' Добро пожаловать на мой квиз');
 
 let question = prompt ('Первый вопрос. Назовите столицу Италии')
 
@@ -14,11 +14,28 @@ else {
 let question2 = prompt ('Назовите столицу Японии')
 if (question2 ='Япония') {
     alert('Верный ответ');
-    schetchik= schetchik+1;
+    schetchik++;
+}
+else {
+    alert('Ошибочка')
+}
+
+let question3 = prompt ('Назовите столицу Беларуси')
+if (question3 ='Минск') {
+    alert('Верный ответ');
+    schetchik++;
+}
+else {
+    alert('Ошибочка')
+}
+let question4 = prompt('Назовите столицу Германии')
+if (question4 ='Берлин') {
+    alert('Верный ответ');
+    schetchik++;
 }
 else {
     alert('Ошибочка')
 }
 
 
-alert('Количество правильных ответов'+schetchik);
+alert('Количество правильных ответов = '+schetchik);
