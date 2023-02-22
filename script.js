534a94b83e43340ad90e786b9b9d7c891e742572
Aldar <aldarsemenov@mail.ru>
2023-02-22 10:47:45 +0800

    fix

--- script.js
+++ script.js
@@ -9,12 +9,57 @@ if (i%2 ==0){
 //2 zadanie
 let j=1;
 while (true){
-   
     if (j==5){
         break;
     }
     j++;
-    
 }
 alert(j);
+
+
+
+//3 zadanie
+
+
+let pasta = {
+    ing1: 'makaron',
+    ing2: 'cheese',
+    ing3: 'milk',
+    ing4:'chicken',
+    costPrice:70, //себестоимость
+    cost: 180      //цена
+}
+let zharexa = {   
+     //жареха
+     ing1: 'potato',
+     ing2: 'onion',
+     ing3: 'pork',
+    costPrice:90,
+    cost: 190
+}
+let  booza ={
+    ing1: 'farsh',
+    int2: 'testo',
+    costPrice: 25,
+    cost: 55
+}
+let buchler  = {  //бухлер
+    ing1: 'beef',
+    ing2: 'potato',
+    costPrice: 100,
+    cost: 230
+}
+
+let menu = [pasta,zharexa,booza,buchler]; // создание массива
+
+
+for ( i=0;
+    i<menu.length;
+    i++)
+    {
+        let profit= menu([i].cost)-menu([i].costPrice);
+        alert(profit);
+    }
+
+
 
