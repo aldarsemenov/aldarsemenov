63f29a69f98ecb71995f4e2d3b94127d14f22ab8
Aldar <aldarsemenov@mail.ru>
2023-02-22 10:06:32 +0800

    fix

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-for (let i=0;i<=10;i++) {
+for (let i=1;i<=10;i++) {
 if (i%2 ==0){
-    alert(i)
+    alert(i);
 }
 }
+
+
+let j=0;
+while (i<100){
+    if (j===5){
+        break;
+    }
+    j++;
+}
+alert(j);
