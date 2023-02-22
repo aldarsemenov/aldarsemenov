553990d68fc3c90253491d0cc9996d12b70027f5
Aldar <aldarsemenov@mail.ru>
2023-02-22 09:59:26 +0800

    add 10 hw

--- script.js
+++ script.js
@@ -1,44 +1,5 @@
-let schetchik = 0;
-let name = prompt('Как вас зовут?');
-alert(name +'. Добро пожаловать на мой квиз');
-
-let question = prompt ('Первый вопрос. Назовите столицу Италии')
-
-if (question =="Рим") {
-    alert('Верный ответ');
-    schetchik= schetchik+1;
-} else {
-    alert('Ошибочка')
+for (let i=0;i<=10;i++) {
+if (i%2 ==0){
+    alert(i)
 }
-let question2 = prompt ('Назовите столицу Японии')
-if (question2 =="Токио") {
-    alert('Верный ответ');
-    schetchik++;
-} else {
-    alert('Ошибочка')
 }
-
-let question3 = prompt ('Назовите столицу Беларуси')
-if (question3 =="Минск") {
-    alert('Верный ответ');
-    schetchik++;
-} else {
-    alert('Ошибочка')
-}
-let question4 = prompt('Назовите столицу Германии')
-if (question4 =="Берлин") {
-    alert('Верный ответ');
-    schetchik++;
-} else {
-    alert('Ошибочка')
-}
-
-let question5 = prompt('Назовите столицу Польши')
-if (question5 =="Варшава") {
-    alert('Верный ответ');
-    schetchik++;
-} else {
-    alert('Ошибочка')
-}
-
-alert('Спасибо что прошли мой квиз. Количество правильных ответов  = '+schetchik);
