0a72af7ab0d95d57201ed8682e65c8e6921a6618
Aldar <aldarsemenov@mail.ru>
2023-03-07 21:51:45 +0800

    add

--- script.js
+++ script.js
@@ -1,23 +1,106 @@
+//1 zadanie
 
+
+function comparison (a,b){
+   
+    if (a==b){
+        alert ('Вы ввели одинаковые числа')
+    }
+else{
+    if (a<b){
+        alert('Наименьшее число а = '+a);
+    }
+    else {
+        alert('Наименьшее число b = '+b);
+    };
+}
+}
+let a = prompt ('Первое число a');
+let b = prompt ('Второе число b');
+comparison (a,b);
+
+
+//2 zadanie
+
+
+
+
+let pasta = {
+    ing1: 'makaron',
+    ing2: 'cheese',
+    ing3: 'milk',
+    ing4:'chicken',
+    costPrice:70, //себестоимость
+    cost: 180      //цена
+}
+let zharexa = {   
+     //жареха
+     ing1: 'potato',
+     ing2: 'onion',
+     ing3: 'pork',
+    costPrice:90,
+    cost: 190
+}
+let  booza ={
+    ing1: 'farsh',
+    int2: 'testo',
+    costPrice: 25,
+    cost: 55
+}
+let buchler  = { 
+     //бухлер
+    ing1: 'beef',
+    ing2: 'potato',
+    costPrice: 100,
+    cost: 230
+}
+
+let menu = [pasta,zharexa,booza,buchler]; // создание массива
+
+
+let profit=0;
+let raznica=0;
+for (i=0;
+    i<menu.length;
+    i++)
+    {
+        profit= menu[i].cost-menu[i].costPrice;
+        
+        alert('Прибыль с единицы товара = '+profit);
+        raznica=menu[i].length;
+        menu[i].raznica=profit;
+        alert(JSON.stringify(menu[i]));
+    }
+// 3 *
    
     let podjarka = {
-        name: 'Поджарка',
         ing:['potato','farsh','onion'],
         cost:150 }
     let lagman = {
-        name:'Лагман',
         ing:['makaron','potato','beef'],
         cost:200}
     let pizza = {
-        name:'Пицца',
         ing:['cheese','chicken','onion','testo'],
         cost:300}
     let menu2=[podjarka,lagman,pizza];
+
+    let ingPrice ={
+        makaron:10,
+        cheese:20,
+        milk:10,
+        chichen:30,
+        potato:20,
+        onion:10,
+        pork:60,
+        farsh:20,
+        testo:5,
+        beef:80
+    }
     let ingr = ['makaron','cheese','milk','chicken','potato','onion','pork','farsh','testo','beef'];
     let price= [10,20,10,30,20,10,60,20,5,80];
     profit=0;
     let cena=0;
-    let MainMenu= '';
+
     for (i=0;i<menu2.length;i++){
         for (j=0;j<menu2[i].ing.length;j++){
             for (k=0;k<ingr.length;k++){
@@ -26,17 +109,9 @@
                 }
             }
         }
-        //alert('Цена блюда = '+cena);
+        alert('Цена блюда = '+cena);
         profit= menu2[i].cost-cena;
-        //alert('Цена прибыли ='+profit);
-        let bluda='';
-        bluda= `Название : ${menu2[i].name} \n Цена: ${menu2[i].cost}\n`;
-       MainMenu+= bluda;
+        alert('Цена прибыли ='+profit);
         cena=0;
         profit=0;
     }
-    alert(MainMenu);
-   
-   
-   
-    
