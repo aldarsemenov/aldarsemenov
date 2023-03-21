71f73543d9551e52560de77a2edaa93a7f1a23e4
Aldar <aldarsemenov@mail.ru>
2023-03-21 11:11:50 +0800

    fix

--- script.js
+++ script.js
@@ -5,30 +5,23 @@ let a=0;
 let database = {
     admin:'root',
     user:'123'}
-let dlina=Object.keys(database).length; //считает количество ключей в обьекте
-    for (let i=0;i<dlina; i++)
-    { 
-        let users=Object.keys(database); //конвертирует ключи обьекта в массив
-        let pwrd=Object.values(database);//конвертирует значения в массив
-        if ((login==users[i])&&(password==pwrd[i]))
-        { alert('Вы вошли');
-        break;
-        }
-        if ((login==users[i])&&(password!=pwrd[i]))
-        { alert('Неверный пароль');
-        break;
-        }
-        if (login!=users[i])
-        {
-            a=a+1;
-        }
+
+
+    if (database[login])
+    {
+        
+    if (database[login]==password)
+    {
+        alert('Вы вошли')
     }
-    if (a==dlina)
+    else {
+        alert('Неверный пароль');
+    }
+}
+    else 
     {
         alert('Такого пользователя не существует')
     }
-    //alert(database.admin);
-
 
   
     
