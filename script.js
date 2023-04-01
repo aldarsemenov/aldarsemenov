9e73e37a88484aafb9611238c3f3ef38cbba8e19
Aldar <aldarsemenov@mail.ru>
2023-04-01 16:49:07 +0800

    complet hw

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let podjarka = {
     name: 'Поджарка',
     ing:['potato','farsh','onion'],
-    cost:150 }
+    cost:150, 
+    costPrice:0,
+    vegan:'false'}
 let lagman = {
     name:'Лагман',
     ing:['makaron','potato','beef'],
-    cost:200}
+    cost:200, 
+    costPrice:0,
+    vegan:'false'}
 let pizza = {
     name:'Пицца',
     ing:['cheese','chicken','onion','testo'],
-    cost:300}
-let menu2=[podjarka,lagman,pizza];
+    cost:300, 
+    costPrice:0,
+    vegan:'false'}
+let salat = {
+    name:'Овощной салат',
+    ing:['tomato','cucumber'],
+    cost:60,
+    costPrice:0,
+    vegan:'true'}
+let veganKotleti={
+    name:'Капустные котлеты',
+    ing:['cabbage','onion'],
+    cost:90,
+    costPrice:0,
+    vegan:'true'}
+let menu2=[podjarka,lagman,pizza,salat,veganKotleti];
 
 let ingPrice ={
     makaron:10,
@@ -22,23 +40,58 @@ let ingPrice ={
     pork:60,
     farsh:20,
     testo:5,
-    beef:80
+    beef:80,
+    tomato:10,
+    cucumber:10,
+    cabbage:15
 }
 
 
-let mainMenu= '';
-for (i=0;i<menu2.length;i++){
-    
-    let cena=0;
-    for (j=0;j<menu2[i].ing.length;j++){
-         cena = cena+ingPrice[menu2[i].ing[j]];
-    }
-    //alert('Цена блюда = '+cena);
-    let profit= menu2[i].cost-cena;
-    //alert('Цена прибыли ='+profit);
-    let bluda='';
-    bluda= `Название : ${menu2[i].name} \n Цена: ${menu2[i].cost}\n`;
-   mainMenu+= bluda;
-    
-}
-alert(mainMenu);
+
+//1 zadanie
+
+console.log("1 задание\n");
+menu2.forEach(function (item, index, arr) {
+    let costPrice = menu2[index]['ing'].reduce(function (sum, item, index) {
+        return sum + ingPrice[item];
+    }, 0);
+    menu2[index]['costPrice'] = costPrice 
+  
+});
+
+menu2.forEach(function (item, index, arr) {
+    return console.log(`Наименование ${menu2[index]['name']}: Себестоимость  ${menu2[index]['costPrice']}`)
+},0);
+
+
+
+
+//2 zadanie  Используйте `map` , чтобы получить массив только с названием и стоимостью каждого блюда.
+console.log("2 задание\n");
+
+let priceName = menu2.map(function(item, index, arr) {
+   return `${menu2[index]['name']} : ${menu2[index]['cost']}`;});
+console.log(priceName);
+
+
+//3.Определите, есть ли в меню хоть одно вегетарианское блюдо, используя `some`.
+console.log("3 задание\n");
+
+let veganEat = menu2.some(function(item, index, arr) {
+    return  menu2[index]['vegan']=='true' ;
+  });
+  console.log(veganEat)
+ 
+//4.Определите, полностью ли у вас вегетарианское меню с помощью `every`.
+console.log("4 задание\n");
+let allVeganEat = menu2.every(function(item, index, arr) {
+    return  menu2[index]['vegan']=='true' ;
+  });
+  console.log(allVeganEat);
+
+//5.Создайте массив с вегетарианскими блюдами с помощью filter.
+
+let veganMenu = menu2.filter(function(item, index, arr) {
+    return  menu2[index]['vegan']=='true' ;
+  });
+  console.log(veganMenu);
